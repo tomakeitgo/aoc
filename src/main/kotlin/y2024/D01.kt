3668b1cfdf1195d1067f685e05355cfe17c8f5b3
willlowery <will.lowery@tomakeitgo.com>
2024-11-30 22:34:30 -0700

    2024 day 1

---
+++ src/main/kotlin/y2024/D01.kt
@@ -0,0 +1,46 @@
+package com.tomakeitgo.y2024
+
+import com.tomakeitgo.R
+import kotlin.math.absoluteValue
+
+fun main() {
+    val pairs = R
+        .fileLinesAsSequence("/2024/01_input.txt")
+        .map { it.split("\\s+".toRegex()) }
+        .map { it[0].toInt() to it[1].toInt() }
+
+
+
+    partOne(pairs) // 2264607
+    partTwo(pairs) // 19457120
+}
+
+private fun partTwo(pairs: Sequence<Pair<Int, Int>>) {
+    val freq = mutableMapOf<Int, Int>()
+    pairs.forEach {
+        freq.put(it.second, freq.getOrDefault(it.second, 0) + 1)
+    }
+    pairs.map {
+        it.first * freq.getOrDefault(it.first, 0)
+    }
+        .reduce(Int::plus)
+        .also(::println)
+}
+
+private fun partOne(pairs: Sequence<Pair<Int, Int>>) {
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+    pairs.forEach {
+        left.add(it.first);
+        right.add(it.second);
+    }
+    left.sort()
+    right.sort()
+
+    var value = 0;
+    left.forEachIndexed { index, i ->
+        value += (i - right[index]).absoluteValue
+    }
+
+    println(value)
+}
